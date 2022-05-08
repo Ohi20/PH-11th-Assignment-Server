6adaca5dd2590d30541199ff7badc861975dd25f
Ohi97 <ashrafohi20@gmail.com>
2022-05-08 06:48:09 +0600

    new database connencted

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
@@ -13,15 +14,36 @@ app.use(express.json());
 
 
 
-const uri = "mongodb+srv://dbUser:Qy82OJYWAliyH3sG@cluster0.pukoz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pukoz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 client.connect(err => {
   const collection = client.db("test").collection("devices");
-  console.log('db connected');
+  console.log('connected');
   // perform actions on the collection object
   client.close();
 });
 
+
+
+// async function run(){
+//     try{
+//         await client.connect();
+//         const userCollection = client.db("carexpress").collection("users");
+       
+//        app.post('/user', (req, res) =>{
+//            const newUser= req.body;
+//            console.log('adding new user', newUser);
+//            res.send('user data recieved');
+//        })
+//     }
+//     finally{
+//         // await client.close();
+//     }
+// }
+
+// run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('running my server');
